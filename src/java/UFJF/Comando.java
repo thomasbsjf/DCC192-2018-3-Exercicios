360f7e626e470b102bc6847be83ce4dce76e3619
ice <ice@lab06m30.nrc.ice.ufjf.br>
2018-09-25 22:19:18 -0300

    Criação interface Comando - Início modificações MVC

---
+++ src/java/UFJF/Comando.java
@@ -0,0 +1,12 @@
+package UFJF;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Comando {
+
+    void exec(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+    
+}
