1fdefabb3cf197e752625bf1754c2ff1ab92e98d
Thomas Santos <thomasbsjf@gmail.com>
2018-11-04 11:47:17 -0200

    Revert "Revert "Merge branch 'master' of https://github.com/thomasbsjf/DCC192-2018-3-Exercicios""
    This reverts commit 969969a32589eea34e8200489d9e2abe52b16b7b.

---
+++ src/java/Model/LoginJpaController.java
@@ -0,0 +1,164 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import Model.exceptions.NonexistentEntityException;
+import Model.exceptions.PreexistingEntityException;
+import Model.exceptions.RollbackFailureException;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.transaction.UserTransaction;
+
+/**
+ *
+ * @author ice
+ */
+public class LoginJpaController implements Serializable {
+
+    public LoginJpaController(UserTransaction utx, EntityManagerFactory emf) {
+        this.utx = utx;
+        this.emf = emf;
+    }
+    private UserTransaction utx = null;
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(Login login) throws PreexistingEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            utx.begin();
+            em = getEntityManager();
+            em.persist(login);
+            utx.commit();
+        } catch (Exception ex) {
+            try {
+                utx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            if (findLogin(login.getLogin()) != null) {
+                throw new PreexistingEntityException("Login " + login + " already exists.", ex);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(Login login) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            utx.begin();
+            em = getEntityManager();
+            login = em.merge(login);
+            utx.commit();
+        } catch (Exception ex) {
+            try {
+                utx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                String id = login.getLogin();
+                if (findLogin(id) == null) {
+                    throw new NonexistentEntityException("The login with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(String id) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            utx.begin();
+            em = getEntityManager();
+            Login login;
+            try {
+                login = em.getReference(Login.class, id);
+                login.getLogin();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The login with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(login);
+            utx.commit();
+        } catch (Exception ex) {
+            try {
+                utx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Login> findLoginEntities() {
+        return findLoginEntities(true, -1, -1);
+    }
+
+    public List<Login> findLoginEntities(int maxResults, int firstResult) {
+        return findLoginEntities(false, maxResults, firstResult);
+    }
+
+    private List<Login> findLoginEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Login.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Login findLogin(String id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Login.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getLoginCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Login> rt = cq.from(Login.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
--- src/java/UFJF/Filter.java
+++
@@ -1,5 +0,0 @@
-package UFJF;
-
-public interface Filter {
-    
-}
